5dfd3e401675d7fda658d79f4089abdd58a7a59b
DARIO LOPEZ <DARIO LOPEZ@10.255.250.158>
2024-11-08 09:04:47 -0500

    Agregar proyecto

--- src/controlador/flota/ControladorVistaTiquete.java
+++ src/controlador/flota/ControladorVistaTiquete.java
@@ -21,7 +21,7 @@ public class ControladorVistaTiquete {
         this.listaPersonas = serializadora.leerArrayListPersonas();
     }
 
-    public void generarTiquete(int cant, Caseta caseta, Viaje viaje, Tiquete tiquete) throws NoPuntosException, NoExisteElViajeException, ElViajeYaNoEstaDisponibleException, NoAsientosException {
+    public void generarTiquete(int cant, Caseta caseta, Viaje viaje,Cliente cliente, Viaje viaje,String pago) throws NoPuntosException, NoExisteElViajeException, ElViajeYaNoEstaDisponibleException, NoAsientosException {
 
         if (viaje.getFechaSalida().isBefore(LocalDate.now())) {
             throw new ElViajeYaNoEstaDisponibleException();
@@ -30,36 +30,38 @@ public class ControladorVistaTiquete {
         if (total > viaje.getVehiculo().getCantidadPuestos()) {
             throw new NoAsientosException();
         }
-        switch (tiquete.getMetodoPago()) {
+        switch (pago) {
             case "Puntos":
                 for (int i = 0; i < cant; i++) {
-                    guardar(caseta,viaje,tiquete);
+                    Tiquete tiquete = guardar(caseta,viaje,cliente, pago);
                     generarTransferencia(viaje, tiquete);
                 }
                 break;
             case "Dinero":
-                incrementardorDePuntos(tiquete);
                 for (int i = 0; i < cant; i++) {
-                    guardar(caseta,viaje,tiquete);
+                    Tiquete tiquete = guardar(caseta,viaje,cliente, pago);
+                    incrementardorDePuntos(tiquete);
                 }
-                this.serializadora.escribirArrayListCasetas(this.listaCasetas);
                 break;
         }
     }
-    public void guardar(Caseta caseta, Viaje viaje, Tiquete tiquete) {
+    public Tiquete guardar(Caseta caseta, Viaje viaje, Cliente cliente, String pago) {
         for (Caseta casetaAux : this.listaCasetas) {
             if (caseta.getEmpresa().getNit() == casetaAux.getEmpresa().getNit()) {
                 for (Viaje viajeAux : casetaAux.getEmpresa().getViajes()) {
                     if (viajeAux.getIdViaje() == viaje.getIdViaje()) {
+                        modelo.Tiquete.cargarContadorId();
+                        Tiquete tiquete = new Tiquete(LocalDate.now(),cliente,viaje,pago);
+                        modelo.Tiquete.guardarContadorId();
                         viajeAux.getTiquetes().add(tiquete);
                         viaje.getTiquetes().add(tiquete);
                         this.serializadora.escribirArrayListCasetas(this.listaCasetas);
-                        return;
+                        return tiquete;
                     }
                 }
             }
         }
-        System.out.println("No se encontró la caseta o el viaje correspondiente.");
+        return null;
     }
 
     public void incrementardorDePuntos(Tiquete tiquete) {
